95c1decc22e525e3137a31434fa048bf9ee48b6e
Sidduprince@07 <siddeshs044@gmail.com>
2023-02-26 18:31:41 +0530

    added

---
+++ test/calcy.js
@@ -0,0 +1,25 @@
+function addNumbers(){
+    var a=parseInt(document.getElementById("value1").value)
+    var b=parseInt(document.getElementById("value2").value)
+    var c=a+b;    
+    alert("sum="+c);
+    
+}
+function subNumbers(){
+    var a=(document.getElementById("value1").value)
+    var b=(document.getElementById("value2").value)
+    var c=a-b;    
+    alert("difference="+c);
+}
+function mulNumbers(){
+    var a=(document.getElementById("value1").value)
+    var b=(document.getElementById("value2").value)
+    var c=a*b;    
+    alert("product="+c);
+}
+function divNumbers(){
+    var a=(document.getElementById("value1").value)
+    var b=(document.getElementById("value2").value)
+    var c=a/b;    
+    alert("division result="+c);
+}
